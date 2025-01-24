fdfaf3b6bfa42869780d5201a1d75b6a21afd682
Cofficebp <cofficebp@gmail.com>
2025-01-24 19:14:52 +0530

    Search feature functional now

--- themes/ptp-luke-child/pages/page-home.php
+++ themes/ptp-luke-child/pages/page-home.php
@@ -83,16 +83,27 @@ $case_study_section = get_field('case_study_section');
                                     </div>
                                     <h1><?php echo $top_main_video_section["heading"]; ?></h1>
                                     <p><?php echo $top_main_video_section["description"]; ?></p>
+                                    <form action="<?php echo site_url('coach-list'); ?>" method="GET">
                                     <div class="filter-form">
                                         <div class="input-group">
-                                            <input type="text" class="form-control" placeholder="What Sport" aria-label="" aria-describedby="basic-addon1">
+                                            <select name="sport" type="text" class="form-control" placeholder="What Sport" aria-label="" aria-describedby="basic-addon1" style="min-height: 56px;">
+                                                <option value="">What Sport</option>
+                                                <?php
+                                                global $wpdb; 
+                                                $table = $wpdb->prefix.'sports';
+                                                $results = $wpdb->get_results("SELECT * FROM  $table ORDER BY sport_name ASC");
+                                                foreach ($results as $row) { ?>
+                                                  <option value="<?php echo $row->sportID; ?>"><?php echo $row->sport_name; ?></option>
+                                                <?php } ?>
+                                            </select>
                                             <div class="input-group-append">
-                                              <button class="btn btn-success" type="button"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="20" height="20" viewBox="0 0 20 20">
+                                              <button class="btn btn-success" type="submit"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="20" height="20" viewBox="0 0 20 20">
                                                 <path fill="#000000" d="M18.869 19.162l-5.943-6.484c1.339-1.401 2.075-3.233 2.075-5.178 0-2.003-0.78-3.887-2.197-5.303s-3.3-2.197-5.303-2.197-3.887 0.78-5.303 2.197-2.197 3.3-2.197 5.303 0.78 3.887 2.197 5.303 3.3 2.197 5.303 2.197c1.726 0 3.362-0.579 4.688-1.645l5.943 6.483c0.099 0.108 0.233 0.162 0.369 0.162 0.121 0 0.242-0.043 0.338-0.131 0.204-0.187 0.217-0.503 0.031-0.706zM1 7.5c0-3.584 2.916-6.5 6.5-6.5s6.5 2.916 6.5 6.5-2.916 6.5-6.5 6.5-6.5-2.916-6.5-6.5z"/>
                                                 </svg></button>
                                             </div>
                                         </div>
                                     </div>
+                                    </form>
                                 </div>
                           </div>
                     </div>
